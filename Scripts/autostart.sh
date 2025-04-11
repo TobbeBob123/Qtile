18ce5f24e768e810a3dd104934bd281dad247626
Tobias Os Finsbol <tobias.finsbol@gmail.com>
2025-04-11 17:30:27 +0200

    removed bloat

--- Scripts/autostart.sh
+++ Scripts/autostart.sh
@@ -11,6 +11,4 @@ signal-desktop &
 blueman-applet &
 /usr/bin/emacs --daemon &
 discord &
-swww init &
-swww img ~/Bakgrunner/Zelda/Zelda_03.jpg &
 autolock -time 30 -locker -corners ++-- 'systemctl suspend' &
