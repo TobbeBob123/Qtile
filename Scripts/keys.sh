e1b61aa4761e3e44bf33ce8dec6f0e794f719404
Tobias Os Finsbol <tobias.finsbol@gmail.com>
2025-05-02 21:32:26 +0200

    Fixed show keys script for better overview

--- Scripts/keys.sh
+++ Scripts/keys.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-sed -n '/Start_keys/,/End_keys/p' ~/.config/qtile/config.py | grep -v '#End_' | grep -v '#Start_' | sed -e 's/Key /\n/' | sed -e 's/# /\n/' | yad --text-info --back=#282a36 --fore=#f8f8f2 --geometry=800x500:
+sed -n '/Start_keys/,/End_keys/p' ~/.config/qtile/config.py | grep -v '#End_' | grep -v '#Start_' | sed -e 's/Key /\n/' | sed -e 's/[]\()\["]//g' | sed 's/mod/Super/g' | sed 's/KeySuper/Key/g' | sed 's/Key/Super /g' | sed 's/,/ +/g' | sed 's/.$//' | sed -e 's/ + lazy[^+]* +/ + /g' | sed 's/+ lazy[^+]* +/ + /g' | sed 's/ +  desc=/ | /g' | sed 's/ + desc=/ | /g' | sed 's/+  desc = /| /g' | sed 's/+  shell=//g' | sed -e 's/# /\n/' | yad --text-info --back=#282a36 --fore=#f8f8f2 --geometry=800x500:
